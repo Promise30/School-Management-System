e1fe8d87cc3ab0d39790b102c635d0678cfa3a69
Promise30 <promiseadeagbo@gmail.com>
2023-07-25 12:12:53 +0100

    initial commit

---
+++ School Management/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School_Management.Models;
+
+namespace School_Management.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Faculty> Faculties { get; set; }
+    }
+}
