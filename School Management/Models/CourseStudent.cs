f20e27c47ecd24861bba3fc081674df7f922d496
Promise30 <promiseadeagbo@gmail.com>
2023-07-25 14:53:09 +0100

    initial commit for junction table model

---
+++ School Management/Models/CourseStudent.cs
@@ -0,0 +1,12 @@
+﻿namespace School_Management.Models
+{
+    public class CourseStudent
+    {
+        // Primary keys as foreign keys to Course and Student
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+    }
+}
