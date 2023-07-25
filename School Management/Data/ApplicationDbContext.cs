99cfb346ae7e9f74d56338f26b0f44b7c690fb9b
Promise30 <promiseadeagbo@gmail.com>
2023-07-25 14:53:34 +0100

    initial commit for database config file

--- School Management/Data/ApplicationDbContext.cs
+++ School Management/Data/ApplicationDbContext.cs
@@ -14,5 +14,30 @@ namespace School_Management.Data
         public DbSet<Teacher> Teachers { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Faculty> Faculties { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CourseStudent>()
+                .HasKey(cs => new { cs.CourseId, cs.StudentId });
+
+            modelBuilder.Entity<CourseStudent>()
+                .HasOne(cs => cs.Course)
+                .WithMany(c => c.CourseStudents)
+                .HasForeignKey(cs => cs.CourseId)
+                .OnDelete(DeleteBehavior.Restrict); // Cascade deletion of Course, delete related records in CourseStudent
+
+            modelBuilder.Entity<CourseStudent>()
+                .HasOne(cs => cs.Student)
+                .WithMany(s => s.CourseStudents)
+                .HasForeignKey(cs => cs.StudentId)
+                .OnDelete(DeleteBehavior.Cascade); // Cascade deletion of Student, delete related records in CourseStudent
+        }
+
+
+
+
     }
+
 }
