d583066cb4c4047649ccc1743cef6ef77cd94c1b
Promise30 <promiseadeagbo@gmail.com>
2023-08-03 04:38:57 +0100

    mapping profiles for the various entities

---
+++ School Management/Helper/MappingProfiles.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using School_Management.Models;
+using School_Management.Models.DTO;
+using School_Management.Models.DTO.CreateDTOs;
+
+namespace School_Management.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Student, StudentDTO>().ReverseMap();
+            CreateMap<CreateStudent, Student>();
+            CreateMap<Course, CourseDTO>().ReverseMap();
+            CreateMap<CreateCourse, Course>();
+            CreateMap<Department, DepartmentDTO>().ReverseMap();
+            CreateMap<CreateDepartment, Department>();
+            CreateMap<Faculty, FacultyDTO>().ReverseMap();
+            CreateMap<CreateFaculty, Faculty>();
+            CreateMap<Teacher, TeacherDTO>().ReverseMap();
+            CreateMap<CreateTeacher, Teacher>();
+        }
+    }
+}
