a1cbfdcecd0d82a01398cd12e56173cce471f685
Promise30 <promiseadeagbo@gmail.com>
2023-08-08 20:40:53 +0100

    updated DTOs by removing the ID fields

--- School Management/Models/DTO/FacultyDTO.cs
+++ School Management/Models/DTO/FacultyDTO.cs
@@ -2,7 +2,7 @@
 {
     public class FacultyDTO
     {
-        public int FacultyId { get; set; }
+
         public string Name { get; set; }
         public string? Description { get; set; }
         public DateTime YearFounded { get; set; }
--- School Management/Models/DTO/TeacherDTO.cs
+++ School Management/Models/DTO/TeacherDTO.cs
@@ -4,7 +4,7 @@ namespace School_Management.Models.DTO
 {
     public class TeacherDTO
     {
-        public int TeacherId { get; set; }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string? MiddleName { get; set; }
