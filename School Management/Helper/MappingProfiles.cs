508fda0f88f9fdde37364f16d43c4d49812244f3
Promise30 <promiseadeagbo@gmail.com>
2023-08-08 20:39:51 +0100

    mapped ApiUser and UserDTO

--- School Management/Helper/MappingProfiles.cs
+++ School Management/Helper/MappingProfiles.cs
@@ -19,6 +19,8 @@ namespace School_Management.Helper
             CreateMap<CreateFaculty, Faculty>();
             CreateMap<Teacher, TeacherDTO>().ReverseMap();
             CreateMap<CreateTeacher, Teacher>();
+            CreateMap<ApiUser, UserDTO>().ReverseMap();
+
         }
     }
 }
