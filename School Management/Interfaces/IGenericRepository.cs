b16594936d6b62cbdc8a3c9d6b0a63dfa3c0c1a5
Promise30 <promiseadeagbo@gmail.com>
2023-08-14 23:28:04 +0100

    Defined the methods in the generic interface

---
+++ School Management/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace School_Management.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll(
+            Expression<Func<T, bool>> expression = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            List<string> includes = null);
+        Task<T> Get(
+            Expression<Func<T, bool>> expression,
+            List<string> includes = null);
+        Task Insert(T entity);
+        Task Delete(int id);
+        void Update(T entity);
+    }
+}
