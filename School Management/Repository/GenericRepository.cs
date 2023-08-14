1f1ddfb7b7e9bf6bc5ec751be95968763929e4fc
Promise30 <promiseadeagbo@gmail.com>
2023-08-14 23:28:58 +0100

    initial implementation of the IGenericRepository methods

---
+++ School Management/Repository/GenericRepository.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School_Management.Data;
+using School_Management.Interfaces;
+using System.Linq.Expressions;
+
+namespace School_Management.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _db = _dbContext.Set<T>();
+        }
+        public async Task Delete(int id)
+        {
+            var entity = await _db.FindAsync(id);
+            _db.Remove(entity);
+        }
+
+
+        public async Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            IQueryable<T> query = _db;
+            if (expression != null)
+            {
+                query = query.Where(expression);
+            }
+
+            if (includes != null)
+            {
+                foreach (var includePropery in includes)
+                {
+                    query = query.Include(includePropery);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+            return await query.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            IQueryable<T> query = _db;
+            if (includes != null)
+            {
+                foreach (var includePropery in includes)
+                {
+                    query = query.Include(includePropery);
+                }
+            }
+            return await query.AsNoTracking().FirstOrDefaultAsync(expression);
+
+        }
+
+        public async Task Insert(T entity)
+        {
+            await _db.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _db.Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+
+        }
+    }
+}
