c5a56eaca94bc587e1026e1edae0b720ab7cfdab
Promise30 <promiseadeagbo@gmail.com>
2023-09-01 15:17:42 +0100

    Provided the method declarations

---
+++ School Management/Interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace School_Management.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "");
+        T GetById(object id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Delete(object id);
+    }
+}
